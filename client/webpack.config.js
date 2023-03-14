17017e71efce082b8e402389b08b274cf7e1325f
Robert Alexander Iatan <robertiatan@gmail.com>
2023-03-13 22:07:56 -0400

    added TODOS

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -69,22 +69,3 @@ module.exports = () => {
     },
   };
 };
-
-module.exports = () => {
-  return {
-    mode: "development",
-    entry: {
-      main: "./src/js/index.js",
-      install: "./src/js/install.js",
-    },
-    output: {
-      filename: "[name].bundle.js",
-      path: path.resolve(__dirname, "dist"),
-    },
-    plugins: [],
-
-    module: {
-      rules: [],
-    },
-  };
-};
